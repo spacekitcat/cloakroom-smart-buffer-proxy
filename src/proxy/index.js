d19c46c92ba129f61d61e6daefda13ab93693f80
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-03-02 02:06:57 +0000

    feat(export): Fixing package export format and some genetal import housekeeping.

---
+++ src/proxy/index.js
@@ -0,0 +1,44 @@
+'use strict'
+
+class Proxy {
+  constructor(maximumSize = 32000) {
+    this.offset = 0;
+    this.internalCache = Buffer.from([]);
+    this.maximumSize = maximumSize
+  }
+
+  append(appendContent) {
+    this.internalCache = Buffer.concat([this.internalCache, appendContent]);
+    const overflow = this.internalCache.length - this.maximumSize;
+    if (overflow > 0) {
+      this.offset -= overflow;
+      this.internalCache = this.internalCache.slice(this.internalCache.length - this.maximumSize);
+    }
+  }
+
+  _translate(index) {
+    const flipped = this.internalCache.length - index - 2;
+    return flipped;
+  }
+
+  getCloakroomTicket(index) {
+    return this._translate(index) + this.offset;
+  }
+
+  readCloakroomTicket(cloakRoomTicket) {
+    const value = this.internalCache[cloakRoomTicket + this.offset + 1];
+    return value ? value : null;
+  }
+
+  getMaximumSize() {
+    return this.maximumSize;
+  }
+
+  getReadOnlyBuffer() {
+    const readOnlyInternalCacheRef = [...this.internalCache];
+    Object.freeze(readOnlyInternalCacheRef);
+    return readOnlyInternalCacheRef;
+  }
+}
+
+export default Proxy;
