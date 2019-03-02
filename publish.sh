ecfb829fcedeec053ba0e42c8d982e02957b13a2
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-03-02 15:44:22 +0000

    fix(publish): Use bash instead.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 git diff-index --quiet HEAD -- || echo "WARNING: THERE ARE UNCOMITTED CHANGES INCLUDED IN THIS PUBLISH OPERATION"; exit;
+
+echo "Preparing release..."
 npm version patch
 git add package.json
 git commit - m "Publising new version";
