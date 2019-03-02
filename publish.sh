0a8283a91a84e8edf044456c3d685c9df63b06f9
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2019-03-02 21:31:26 +0000

    fix(deploy): fixing tagging code

--- publish.sh
+++ publish.sh
@@ -11,6 +11,6 @@ echo "Preparing release..."
 VERSION=$(npm version patch)
 git add package.json
 git commit -m "Publising $VERSION";
-git tag -1 $VERSION
+git tag -a $VERSION -m "Releasing $VERSION"
 git push --tags
 npm publish
